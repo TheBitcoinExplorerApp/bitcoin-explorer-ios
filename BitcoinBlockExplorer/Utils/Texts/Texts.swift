6f075dc72871ac3c51ff22ab0b401934f248a3e2
Victor Hugo <vhugopacheco59@gmail.com>
2025-06-12 15:34:42 -0300

    views de doacao retiradas

--- BitcoinBlockExplorer/Utils/Texts/Texts.swift
+++ BitcoinBlockExplorer/Utils/Texts/Texts.swift
@@ -61,21 +61,6 @@ extension Texts {
     static let termsLabel: String = String(localized: "termsLabel")
 }
 
-// Donations Labels
-extension Texts {
-    static let scanText1 = String(localized: "Scan the Bitcoin QR Code with your bitcoin wallet to help me improving the app.")
-    
-    static let copy = String(localized: "Copy address")
-    
-    static let scanText2 = String(localized: "Scan the lightning network QR Code with your lightning wallet to help me improving the app.")
-    
-    static let copyInvoice = String(localized: "Copy lightning invoice")
-    
-    static let bitcoinAddressTitle = String(localized: "Bitcoin mainnet address")
-    
-    static let lightningTitle = String(localized: "Lightning network address")
-}
-
 // ToastView labels
 extension Texts {
     static let copied: String = String(localized: "copied")
