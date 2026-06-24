952d44f97abf6a63a87f77906c49ae626fe51324
Victor Hugo <vhugopacheco59@gmail.com>
2026-06-24 10:11:54 -0300

    dismiss modal atualizado

--- BitcoinBlockExplorer/Utils/Modifiers/SheetToolbarModifier.swift
+++ BitcoinBlockExplorer/Utils/Modifiers/SheetToolbarModifier.swift
@@ -19,19 +19,23 @@ struct SheetToolbarModifier: ViewModifier {
                 .navigationBarTitleDisplayMode(.inline)
                 .toolbar {
                     ToolbarItem(placement: .topBarTrailing) {
-                        Button{
-                            dismiss()
-                        } label: {
-                            Circle()
-                                .fill()
-                                .foregroundStyle(Color.dismissBackground)
-                                .frame(width: 30, height: 30)
-                                .overlay() {
-                                    Text("X")
-                                        .clipShape(Circle())
-                                        .font(.system(size: 22.5))
-                                        .foregroundColor(Color.primaryText)
-                                }
+                        if #available(iOS 26, *) {
+                            Button(role: .close) { dismiss() }
+                        } else {
+                            Button{
+                                dismiss()
+                            } label: {
+                                Circle()
+                                    .fill()
+                                    .foregroundStyle(Color.dismissBackground)
+                                    .frame(width: 30, height: 30)
+                                    .overlay() {
+                                        Text("X")
+                                            .clipShape(Circle())
+                                            .font(.system(size: 22.5))
+                                            .foregroundColor(Color.primaryText)
+                                    }
+                            }
                         }
                     }
                     
@@ -41,7 +45,8 @@ struct SheetToolbarModifier: ViewModifier {
                             .bold()
                             .font(.headline)
                     }
-                }.toolbarBackground(Color.myBackground, for: .navigationBar)
+                }
+                .toolbarBackgroundIfNeeded(Color.myBackground)
         }
     }
 }
