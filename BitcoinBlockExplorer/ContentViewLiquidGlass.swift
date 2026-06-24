657a866d756bc0100703f646588d38b1fb00a241
Victor Hugo <vhugopacheco59@gmail.com>
2026-06-24 16:37:36 -0300

    ScrollViewReader removido ios 26

--- BitcoinBlockExplorer/ContentViewLiquidGlass.swift
+++ BitcoinBlockExplorer/ContentViewLiquidGlass.swift
@@ -10,67 +10,54 @@ import SwiftUI
 struct ContentViewLiquidGlass: View {
     @StateObject var viewModel = BlockchainViewModel()
     @StateObject var searchModel = SearchModel()
-
-    @State private var tabSelection = 1
-    @State private var tappedTwice: Bool = false
     
     @SceneStorage("selectedTab") var selectedTab = 0
     
     var body: some View {
         
-        return ScrollViewReader { proxy in
-            if #available(iOS 26.0, *) {
-                TabView(selection: $selectedTab) {
-                    Tab(Texts.blockchain, systemImage: "cube.fill", value: 1) {
-                        NavigationStack {
-                            BlockchainView()
-                                .onChange(of: tappedTwice, perform: { tapped in
-                                    if tapped {
-                                        withAnimation {
-                                            proxy.scrollTo(1, anchor: .top)
-                                        }
-                                        tappedTwice = false
-                                    }
-                                })
-                                .environmentObject(viewModel)
-                        }
-                        .tag(1)
+        if #available(iOS 26.0, *) {
+            TabView(selection: $selectedTab) {
+                Tab(Texts.blockchain, systemImage: "cube.fill", value: 1) {
+                    NavigationStack {
+                        BlockchainView()
+                            .environmentObject(viewModel)
                     }
-                   
-                    Tab(value: 2) {
-                        NavigationStack {
-                            CalculatorView()
-                        }
-                        .tag(2)
-                    } label: {
-                        Label {
-                            Text(Texts.calculator)
-                        } icon: {
-                            Image("calculatorIcon")
-                                .renderingMode(.template)
-                                .foregroundStyle(Color.accentColor)
-                        }
+                    .tag(1)
+                }
+                
+                Tab(value: 2) {
+                    NavigationStack {
+                        CalculatorView()
                     }
-
-                    Tab(Texts.configuracoes, systemImage: "gearshape.fill", value: 3) {
-                        
-                        NavigationStack {
-                            ConfigurationsView()
-                        }
-                        .tag(3)
-
+                    .tag(2)
+                } label: {
+                    Label {
+                        Text(Texts.calculator)
+                    } icon: {
+                        Image("calculatorIcon")
+                            .renderingMode(.template)
+                            .foregroundStyle(Color.accentColor)
                     }
+                }
+                
+                Tab(Texts.configuracoes, systemImage: "gearshape.fill", value: 3) {
                     
-                    Tab(value: 4, role: .search) {
-                        NavigationStack {
-                            SearchTabView(model: searchModel)
-                        }
-                        .tag(4)
+                    NavigationStack {
+                        ConfigurationsView()
                     }
+                    .tag(3)
                     
                 }
-                .accentColor(Color.primaryText)
+                
+                Tab(value: 4, role: .search) {
+                    NavigationStack {
+                        SearchTabView(model: searchModel)
+                    }
+                    .tag(4)
+                }
+                
             }
+            .accentColor(Color.primaryText)
         }
         
     }
